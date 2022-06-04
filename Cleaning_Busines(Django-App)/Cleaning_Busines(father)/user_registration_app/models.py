434b3de26a8dd9078a867fb248bd25d1c85e91e6
Mike <98065991+Mike-Ehl@users.noreply.github.com>
2022-06-04 16:48:15 -0400

    Delete Cleaning_Busines(Django-App)/Cleaning_Busines(father) directory

--- Cleaning_Busines(Django-App)/Cleaning_Busines(father)/user_registration_app/models.py
+++
@@ -1,9 +0,0 @@
-from re import M
-from django.db import models
-
-
-
-class Picture(models.Model):
-    name = models.CharField(max_length=100)
-    url = models.CharField(max_length=100) 
-    onemorefield = models.DateField()
